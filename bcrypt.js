0341bddb7b2f8364b90e3460fda8d124e4397a1e
lemoinemeddy <53619610+lemoinemeddy@users.noreply.github.com>
2019-08-04 19:01:40 +0200

    Update bcrypt.js

--- bcrypt.js
+++ bcrypt.js
@@ -1,33 +1,23 @@
 // Lemoine Meddy 2019, can be fork ;)
 
 const bcrypt = require ('bcrypt')
+
+function connect(password, username, cb){
 const iteration = 10
-
-class Want {
-
-static getHash(password, cb){
-bcrypt.hash(password, iteration, function(err, hash){
-  if(err) throw err
-cb( password.length <= 72  ? hash : "needed small password" )
-})
-}
-static getMatch(password, hash, cb){
-bcrypt.compare(password, hash, function(err, res){
+password.length <= 72 ? bcrypt.hash(password, iteration, (err, hash_input) => {
+if(err) throw err
+// get hash for username from bdd
+let hash_bdd =  ''
+bcrypt.compare(hash_bdd, hash_input, (err, res) => {
 if(err) throw err
 cb(res)
 })
-}
+}) : "needed small password" //if moore than 72
 }
 
+let username = "meddy"
 let password = "Hellodude"
-let hash = "" //DEMO
 
-// Get Hash, from example user input
-Want.getHash(password , function(rephash){
-console.log(rephash)
-})
-
-// Compare is match with password & hash
-Want.getMatch(password , hash, function(match){
-console.log(match)
+connect(password, username, hash_bdd, (reponse) => {
+  console.log(reponse)
 })
